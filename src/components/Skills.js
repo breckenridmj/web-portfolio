296074aeed60fe8ba9cdc5e87fa5520913804bb3
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-28 12:56:38 -0700

    updated carousel meters

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,13 @@
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png";
 
 
-export const Skills => () => {
+export const Skills = () => {
     const responsive = {
         superLargeDesktop: {
             // the naming can be any, depends on you.
@@ -32,6 +36,24 @@ export const Skills => () => {
                         <div className="skill-bx">
                             <h2>Skills</h2>
                             <p> jdlajfldasjdfladjfladjlfajald</p>
+                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                                <div className="item">
+                                    <img src={meter1} alt="Image" />
+                                    <h5>Web Development</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter2} alt="Image" />
+                                    <h5>Web Development</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter3} alt="Image" />
+                                    <h5>Web Development</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter1} alt="Image" />
+                                    <h5>Web Development</h5>
+                                </div>
+                            </Carousel>
 
                         </div>
                     </Col>
