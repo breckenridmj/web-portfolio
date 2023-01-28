e947aca2d62e88ce132198f8faeb2ab6c62a1188
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-28 12:59:15 -0700

    added colorSHarp to skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -59,6 +59,7 @@ export const Skills = () => {
                     </Col>
                 </Row>
             </Container>
+            <img className="background-image-left" src={colorSharp}/>
         </section>
     )
 
