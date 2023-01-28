fc66c2cb0dd414eb526e6b8a5277d38f8812a48b
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-27 23:56:04 -0700

    creating navbar

---
+++ src/components/NavvBar.js
@@ -0,0 +1,15 @@
+
+// import Navbar and and container from react bootstrap
+// link to navbar component for bootstrap https://react-bootstrap.github.io/components/navbar/
+import {Navbar, Container} from "react-bootstrap";
+
+//created a functional component
+export const NavBar = () => {
+
+    //returning functional component
+    return(
+
+
+
+    )
+}
