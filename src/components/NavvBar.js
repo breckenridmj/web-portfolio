e04bfa8602dbd3b56b91a6865c7a9327c8c88bbb
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-28 00:30:20 -0700

    update navbar

--- src/components/NavvBar.js
+++ src/components/NavvBar.js
@@ -10,31 +10,45 @@ export const NavBar = () => {
     return (
 
         // copied bootstrap component 
-        // SHIFT+ALT+F (autoformats)
-        <Navbar bg="light" expand="lg">
+        // SHIFT+ALT+F (auto-formats)
+        <Navbar expand="lg">
+
+            //The container classes are flexbox classes that allow you to create a grid system.
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Brand href="#home">
+                    //image
+                    <img src={''} alt="Logo" />
+                </Navbar.Brand>
+                
+                //img on mobile menu is minimized
+                <Navbar.Toggle aria-controls="basic-navbar-nav"> 
+                    <span className="navbar-toggler-icon"></span>
+                </Navbar.Toggle>
+
                 <Navbar.Collapse id="basic-navbar-nav">
+                    //Navbar links
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#skills">Skills</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
                     </Nav>
+                    
+                    //Social Icons
+                    <span className="navbar-text">
+                        <div className="social-icon">
+                            <a href="#"><img src={} alt="" /></a>
+                            <a href="#"><img src={} alt="" /></a>
+                            <a href="#"><img src={} alt="" /></a>
+                        </div>
+
+                        //button that for the contact form
+                        <button className="vvd" onClick={() => console.log('connect')}>
+                            <span> Let's Connect</span>
+                        </button>
+                    </span>
+
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-
-
     )
 }
