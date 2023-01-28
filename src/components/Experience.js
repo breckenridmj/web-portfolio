06d9e0508c7b1c910c1a684313efda8abb745cfe
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-28 13:29:54 -0700

    Created Experience js

---
+++ src/components/Experience.js
@@ -0,0 +1,21 @@
+
+import { Container, Row, Col } from "react-bootstrap"
+
+
+
+
+export const Banner = () => {
+    return (
+        <section>
+            <Container>
+                <Row>
+                    <Col>
+                    
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+
+    )
+    
+}
