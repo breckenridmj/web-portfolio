1ad56263e2d928837c2d86280b85c22400ebe803
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-28 11:16:37 -0700

    added Banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './assets/img/Dev_logo.png';
 import './App.css';
 import {NavBar} from './components/NavBar';
+import {Banner} from './components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Banner/>
     </div>
   );
 }
