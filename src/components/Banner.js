71d29e7551f3ebe5c73ccc622ea6e48de2cb2f60
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-28 16:56:15 -0700

    updated banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -53,7 +53,8 @@ export const Banner = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <TrackVisibility>
-                            <div className={isVisible}>
+                        {({isVisible}) =>
+                            <div className={isVisible ? "animated_animated animate_fadeIn" : ""}>
                                 <span className="tagline">Welcome to my Portfolio</span>
                                 <h1>{'Hi, Im Malcolm Breckenridge '}<br></br><span className="wrap">{text}</span></h1>
                                 <p> 
@@ -63,6 +64,7 @@ export const Banner = () => {
                                 </p>
                                 <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25} /></button>
                             </div>
+                            }
                         </TrackVisibility>
                     </Col>
 
