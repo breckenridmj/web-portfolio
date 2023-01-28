de59bff0aa8a2e73530f1717cf0e5d6fb57e1f2a
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-28 11:22:09 -0700

    updated Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,13 @@ export const Banner = () => {
                 <Row className="align-items-center">
                     <col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{'Hi I'm webdecoded'}<span className="wrap">web developer</span></h1>
+                        <h1>{'Hi Im webdecoded'}<span className="wrap">web developer</span></h1>
+                        <p> 
+                            Highly motivated and enthusiastic software engineer with a background in Mechanical 
+                            Engineering and Computer Science looking to utilize my coding skills to create 
+                            meaningful software applications. Offering expertise in Java, HTML, C++, Node.js, 
+                            JavaScript, and SQL databases. 
+                        </p>
                     </col>
                 </Row>
             </Container>
