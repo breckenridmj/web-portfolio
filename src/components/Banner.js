2ce36668674b42e5b94d1f40a7c7145a72e1c2da
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-28 11:52:52 -0700

    updated Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,21 +1,68 @@
-import { Container, Row } from "react-bootstrap"
+import {useState, useEffect} from "react";
+import { Container, Row, Col } from "react-bootstrap"
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import HeaderImg from "../assets/img/header-img.svg"
 
 
 export const Banner = () => {
+    
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+    const [text, setText] = useState('');
+    const [delta, setDelta] useState(300 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            ticker();
+        },delta)
+
+        return () => {clearInterval(ticker)};
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.lenght + 1);
+
+        setText(updatedText);
+
+        if (isDeleting){
+            setDelta(prevDelta => prevDelta /2)
+        }
+
+        if (!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }else if(isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+    
+    
     return (
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
-                    <col xs={12} md={6} xl={7}>
+                    <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{'Hi Im webdecoded'}<span className="wrap">web developer</span></h1>
+                        <h1>{'Hi Im webdecoded'}<span className="wrap">{text}</span></h1>
                         <p> 
                             Highly motivated and enthusiastic software engineer with a background in Mechanical 
                             Engineering and Computer Science looking to utilize my coding skills to create 
                             meaningful software applications. Offering expertise in Java, HTML, C++, Node.js, 
                             JavaScript, and SQL databases. 
                         </p>
-                    </col>
+                        <button onClick={() => console.log('connect')}>Let's Connect</button>
+                    </Col>
+
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={HeaderImg} alt="Header Img" />
+
+                    </Col>
                 </Row>
             </Container>
         </section>
