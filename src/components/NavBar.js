ca254d54828ad9362ca12ffed80017c13e2bd388
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-28 03:26:20 -0700

    updated navbar comments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import {useState, useEffect} from "react";
 
 // import Navbar and and container from react bootstrap
 // link to navbar component for bootstrap https://react-bootstrap.github.io/components/navbar/
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 // import logo from assets folder
 import logo from '../assets/img/logo.svg';
@@ -57,30 +57,31 @@ export const NavBar = () => {
         // className based on what condition the scroll is (if its scrolled name it scrolled if its not leave it blank)
         <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
 
-            //The container classes are flexbox classes that allow you to create a grid system.
+            {/*The container classes are flexbox classes that allow you to create a grid system.*/}
             <Container>
                 <Navbar.Brand href="#home">
-                    //image
+                    {/*image*/}
                     <img src={logo} alt="Logo" />
                 </Navbar.Brand>
                 
-                //img on mobile menu is minimized
+                {/* img on mobile menu is minimized*/}
                 <Navbar.Toggle aria-controls="basic-navbar-nav"> 
                     <span className="navbar-toggler-icon"></span>
                 </Navbar.Toggle>
 
                 <Navbar.Collapse id="basic-navbar-nav">
-                    //Navbar links
+                    {/* Navbar links */}
                     <Nav className="me-auto">
-                        // class name is set to active navbar-link when active link is equal to "home" & if its not set to just navbar-link 
+                        {/* class name is set to active navbar-link when active link is equal to "home" & if its not set to just navbar-link 
                         // onClick function updates the name of the link to "home" 
-                        // same goes for each link 
+                        // same goes for each link */}
+                        
                         <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}> Home </Nav.Link>
                         <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}> Skills </Nav.Link>
                         <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}> Projects </Nav.Link>
                     </Nav>
                     
-                    //Social Icons
+                    {/*Social Icons*/}
                     <span className="navbar-text">
                         <div className="social-icon">
                             <a href="#"><img src={navIcon1} alt="" /></a>
@@ -88,7 +89,7 @@ export const NavBar = () => {
                             <a href="#"><img src={navIcon3} alt="" /></a>
                         </div>
 
-                        //button that for the contact form
+                        {/*button that for the contact form*/}
                         <button className="vvd" onClick={() => console.log('connect')}>
                             <span> Let's Connect</span>
                         </button>
