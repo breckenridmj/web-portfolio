fd1ce6ada3d1bde86965dcfa7f3a4dbf532ec49e
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-28 18:32:54 -0700

    added env file with mailchimp info

---
+++ src/components/MailchimpForm.js
@@ -0,0 +1,23 @@
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import { Newsletter } from "./Newsletter";
+
+export const MailchimpForm = () => {
+  const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
+
+  return (
+    <>
+      <MailchimpSubscribe
+        url={postUrl}
+        render={({ subscribe, status, message }) => (
+          <Newsletter
+            status={status}
+            message={message}
+            onValidated={formData => subscribe(formData)}
+            />
+        )}
+        />
+    </>
+  )
+}
+<input type="hidden" name="u" value="5a816eacb891b27d87d881d4f"></input>
+<input type="hidden" name="id" value="3446f6dc75"></input>
