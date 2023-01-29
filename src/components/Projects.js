2d8439cba86d33e313e538cccb5a0f468bf4d2d6
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-28 19:11:40 -0700

    updated project .js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -52,6 +52,8 @@ export const Projects = () => {
                                 <div className={isVisible ? "animate__animated animate__bounce" : ""}>
                                     <h2>Projects</h2>
                                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                    </div>}
+                        </TrackVisibility>
                                     <Tab.Container id="projects-tabs" defaultActiveKey="first">
                                         <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                                             <Nav.Item>
@@ -87,8 +89,8 @@ export const Projects = () => {
                                             </Tab.Pane>
                                         </Tab.Content>
                                     </Tab.Container>
-                                </div>}
-                        </TrackVisibility>
+                               
+                        
                     </Col>
                 </Row>
             </Container>
