1850794bd6a5caf32ffe41730c93f0e24c689822
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-28 19:28:24 -0700

    got links working

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,11 +6,11 @@ import { Container, Row, Col } from "react-bootstrap";
 
 export const Experience = () => {
     return (
-        <section>
+        <section className="experience" id="experience">
             <Container>
                 <Row>
                     <Col>
-                        <h1>hi</h1>
+                        <h1>Work Experience</h1>
                     </Col>
                 </Row>
             </Container>
