d2cf36ece085e08afa2930586a03cc98fed0699f
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-28 19:47:39 -0700

    updated social links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -85,9 +85,9 @@ export const NavBar = () => {
                     {/*Social Icons*/}
                     <span className="navbar-text">
                         <div className="social-icon">
-                            <a href="#"><img src={navIcon1} alt="" /></a>
-                            <a href="#"><img src={navIcon2} alt="" /></a>
-                            <a href="#"><img src={navIcon3} alt="" /></a>
+                            <a href="https://www.linkedin.com/in/malcolmbreckenridge/"><img src={navIcon1} alt="linkedIn" /></a>
+                            <a href="https://www.facebook.com/BreckenridgeInvestmentGroup"><img src={navIcon2} alt="facebook" /></a>
+                            <a href="https://www.instagram.com/malcolmbreckenridge/"><img src={navIcon3} alt="instagram" /></a>
                         </div>
 
                         {/*button that for the contact form*/}
