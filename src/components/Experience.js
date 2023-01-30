1ebaffd52f4bbafe7b223c27ad6d1ac527514bba
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-29 22:10:42 -0700

    updated css

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,13 +4,32 @@ import { Container, Row, Col } from "react-bootstrap";
 
 
 
+
 export const Experience = () => {
     return (
         <section className="experience" id="experience">
             <Container>
-                <Row>
-                    <Col>
-                        <h1>Work Experience</h1>
+                <Row className="align-items-center">
+                    <h1>Work Experience</h1>
+                    <Col md={6}>                      
+                        <h2>IT Database Administrator</h2>
+                    </Col>
+                    <Col size={12} sm={6}>
+                        <p>
+                            IT Database AdministratorIT Database Administrator
+                            General Dynamics Information Technology · Full-timeGeneral Dynamics Information Technology · Full-time
+                            Jan 2023 - Present · 1 moJan 2023 - Present · 1 mo
+                            Sierra Vista, Arizona, United StatesSierra Vista, Arizona, United States
+                            • Resolved problems associated with SQL server and SharePoint system data
+                            • Gathered data, designs formats, and created new programs or adjusted existing 
+                            programs as needed to create reports while documenting all changes
+                            • Analyze and troubleshoot software database systems and performance problems 
+                            and accurately document actions taken
+                            • Review quarterly Security Technical Implementation Guides (STIGs), remediate 
+                            open vulnerabilities and report compliance
+
+
+                        </p>
                     </Col>
                 </Row>
             </Container>
