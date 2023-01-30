5ca553af5fce2581d612975ba77587f196927730
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-29 23:35:13 -0700

    updated job details

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -15,11 +15,11 @@ export const Experience = () => {
               <p class="text-muted">
                 • Resolved problems associated with SQL server and SharePoint system data<br/>
                 • Gathered data, designs formats, and created new programs or adjusted existing <br/>
-                programs as needed to create reports while documenting all changes<br/>
+                &nbsp;&nbsp;programs as needed to create reports while documenting all changes<br/>
                 • Analyze and troubleshoot software database systems and performance problems <br/>
-                    and accurately document actions taken<br/>
+                &nbsp;&nbsp;and accurately document actions taken<br/>
                 • Review quarterly Security Technical Implementation Guides (STIGs), remediate <br/>
-                    open vulnerabilities and report compliance
+                &nbsp;&nbsp;open vulnerabilities and report compliance
               </p>
             </li>
         
@@ -29,8 +29,8 @@ export const Experience = () => {
               <p class="text-muted">
                 • Proposed robotic solutions to technical and performance issues<br/>
                 • Ensured fabricated robots met all company standards<br/>
-                • Assist in verifications and hardware inspections to ensure the machine <br/>meets all technical 
-                specification <br/>
+                • Assist in verifications and hardware inspections to ensure the machine <br/>&nbsp;&nbsp;meets all technical 
+                &nbsp;&nbsp;specification <br/>
                 • Troubleshoot, diagnose, and correct malfunctions by modifications<br/>
                 • Maintained robots for automation of manufacturing processes<br/>
                 • Installation of the robotic machines and product instructions to clients
@@ -41,10 +41,10 @@ export const Experience = () => {
               <h5 class="fw-bold">Front End Web Developer - <span>Prive Wealth Club</span></h5>
               <p class="text-muted mb-2 fw-bold">July 2021 - July 2022<br/>Richmond, Virginia</p>
               <p class="text-muted">
-                • Maintain websites and applications, including content updates, account <br/>administration, debugging, feature enhancements and documentations, <br/>testing, and regular upgrades to ensure web security and system usability.<br/>
-                • Identify current internet and website requirements, and participate in <br/>planning and execution of projects by organizing, scheduling, executing, <br/>and coordinating work assignments to achieve milestone deadlines.<br/>
-                • Support maintenance programming development for web pages and <br/>web applications.<br/>
-                • Research new web technologies, including software and hardware <br/>developments, and recommend purchases and changes in design, <br/>security, software, and hardware.
+                • Maintain websites and applications, including content updates, account <br/>&nbsp;&nbsp;administration, debugging, feature enhancements and documentations, <br/>&nbsp;&nbsp;testing, and regular upgrades to ensure web security and system usability.<br/>
+                • Identify current internet and website requirements, and participate in <br/>&nbsp;&nbsp;planning and execution of projects by organizing, scheduling, executing, <br/>&nbsp;&nbsp;and coordinating work assignments to achieve milestone deadlines.<br/>
+                • Support maintenance programming development for web pages and <br/>&nbsp;&nbsp;web applications.<br/>
+                • Research new web technologies, including software and hardware <br/>&nbsp;&nbsp;developments, and recommend purchases and changes in design, <br/>&nbsp;&nbsp;security, software, and hardware.
               </p>
             </li>
         
@@ -52,17 +52,17 @@ export const Experience = () => {
               <h5 class="fw-bold">IT Help Desk Technician - <span>GPM Investments, LLC</span></h5>
               <p class="text-muted mb-2 fw-bold">March 2021 - June 2021<br/>Richmond, Virginia</p>
               <p class="text-muted">
-                • Assist in the architecture, implementation and maintenance of the <br/>organization's desktop infrastructure environments including installation, <br/>upgrades, high level troubleshooting, patching, and software deployment<br/>
-                • Install Windows 10 Base Image, crafting the operating system and GPO <br/>settings so that they integrate with all enterprise hardware and software.<br/>
-                • Serve as the first point of contact for customers seeking technical <br/>assistance over the phone or email<br/>
-                • Perform remote troubleshooting through diagnostic techniques and <br/>pertinent questions<br/>
-                • Determine the best solution based on the issue and details provided by <br/>customers<br/>
+                • Assist in the architecture, implementation and maintenance of the <br/>&nbsp;&nbsp;organization's desktop infrastructure environments including installation, <br/>&nbsp;&nbsp;upgrades, high level troubleshooting, patching, and software deployment<br/>
+                • Install Windows 10 Base Image, crafting the operating system and GPO <br/>&nbsp;&nbsp;settings so that they integrate with all enterprise hardware and software.<br/>
+                • Serve as the first point of contact for customers seeking technical <br/>&nbsp;&nbsp;assistance over the phone or email<br/>
+                • Perform remote troubleshooting through diagnostic techniques and <br/>&nbsp;&nbsp;pertinent questions<br/>
+                • Determine the best solution based on the issue and details provided by <br/>&nbsp;&nbsp;customers<br/>
                 • Walk the customer through the problem-solving process<br/>
                 • Direct unresolved issues to the next level of support personnel<br/>
                 • Follow-up and update customer status and information<br/>
-                • Pass on any feedback or suggestions by customers to the appropriate <br/>internal team<br/>
+                • Pass on any feedback or suggestions by customers to the appropriate <br/>&nbsp;&nbsp;internal team<br/>
                 • Identify and suggest possible improvements on procedures<br/>
-                • Manage inventory as well as warehouse, transportation, and other <br/>logistics processes
+                • Manage inventory as well as warehouse, transportation, and other <br/>&nbsp;&nbsp;logistics processes
               </p>
             </li>
           </ul>
