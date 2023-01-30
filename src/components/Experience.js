2396246d95bc1d68bf52e884edbc0aae2d9f966f
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-29 22:47:00 -0700

    added work Experience timeline

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,38 +3,59 @@ import { Container, Row, Col } from "react-bootstrap";
 
 
 
-
-
 export const Experience = () => {
     return (
-        <section className="experience" id="experience">
-            <Container>
-                <Row className="align-items-center">
-                    <h1>Work Experience</h1>
-                    <Col md={6}>                      
-                        <h2>IT Database Administrator</h2>
-                    </Col>
-                    <Col size={12} sm={6}>
-                        <p>
-                            IT Database AdministratorIT Database Administrator
-                            General Dynamics Information Technology · Full-timeGeneral Dynamics Information Technology · Full-time
-                            Jan 2023 - Present · 1 moJan 2023 - Present · 1 mo
-                            Sierra Vista, Arizona, United StatesSierra Vista, Arizona, United States
-                            • Resolved problems associated with SQL server and SharePoint system data
-                            • Gathered data, designs formats, and created new programs or adjusted existing 
-                            programs as needed to create reports while documenting all changes
-                            • Analyze and troubleshoot software database systems and performance problems 
-                            and accurately document actions taken
-                            • Review quarterly Security Technical Implementation Guides (STIGs), remediate 
-                            open vulnerabilities and report compliance
-
-
-                        </p>
-                    </Col>
-                </Row>
-            </Container>
+       
+        <section class="py-5">
+            <h1>Work Experience</h1>
+          <ul class="timeline">
+            <li class="timeline-item mb-5">
+              <h5 class="fw-bold">Our company starts its operations</h5>
+              <p class="text-muted mb-2 fw-bold">11 March 2020</p>
+              <p class="text-muted">
+                Lorem ipsum dolor sit amet consectetur <br/>adipisicing elit. Sit
+                necessitatibus adipisci, ad alias, <br/>voluptate pariatur officia
+                repellendus repellat inventore fugit <br/>perferendis totam dolor
+                voluptas et corrupti distinctio maxime <br/>corporis optio?
+              </p>
+            </li>
+        
+            <li class="timeline-item mb-5">
+              <h5 class="fw-bold">First customer</h5>
+              <p class="text-muted mb-2 fw-bold">19 March 2020</p>
+              <p class="text-muted">
+                Quisque ornare dui nibh, sagittis egestas <br/>nisi luctus nec. Sed
+                aliquet laoreet sapien, eget pulvinar<br/> lectus maximus vel.
+                Phasellus suscipit porta mattis.
+              </p>
+            </li>
+        
+            <li class="timeline-item mb-5">
+              <h5 class="fw-bold">Our team exceeds 10 people</h5>
+              <p class="text-muted mb-2 fw-bold">24 June 2020</p>
+              <p class="text-muted">
+                Orci varius natoque penatibus et magnis dis<br/> parturient montes,
+                nascetur ridiculus mus. Nulla ullamcorper <br/>arcu lacus, maximus
+                facilisis erat pellentesque nec. Duis et <br/>dui maximus dui aliquam
+                convallis. Quisque consectetur purus erat, <br/>et ullamcorper sapien
+                tincidunt vitae.
+              </p>
+            </li>
+        
+            <li class="timeline-item mb-5">
+              <h5 class="fw-bold">Earned the first million $!</h5>
+              <p class="text-muted mb-2 fw-bold">15 October 2020</p>
+              <p class="text-muted">
+                Nulla ac tellus convallis, pulvinar nulla ac, <br/>fermentum diam. Sed
+                et urna sit amet massa dapibus tristique non <br/>finibus ligula. Nam
+                pharetra libero nibh, id feugiat tortor rhoncus <br/>vitae. Ut suscipit
+                vulputate mattis.
+              </p>
+            </li>
+          </ul>
         </section>
+      
 
     )
-    
+
 }
