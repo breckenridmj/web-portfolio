2645940ec404229da9d4921b4c41f5f4cdc4229d
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-01-31 08:56:14 -0700

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {Banner} from './components/Banner';
 import {Skills} from './components/Skills';
 import {Projects} from './components/Projects';
 import {Experience} from './components/Experience';
-import {Model} from './components/Model';
 import {Footer} from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -17,7 +16,6 @@ function App() {
       <Skills/>
       <Projects/>
       <Experience/>
-      <Model/>
       <Footer/>
     </div>
   );
