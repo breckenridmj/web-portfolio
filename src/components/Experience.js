f48eb146abd3be8e01c1101741a8fab54b584156
Malcolm Breckenridge <59925642+breckenridmj@users.noreply.github.com>
2023-02-02 19:54:58 -0700

    unmuted text in div for experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -11,8 +11,8 @@ export const Experience = () => {
           <ul class="timeline">
             <li class="timeline-item mb-5">
               <h5 class="fw-bold">IT Database Administrator - <span>General Dynamics Information Technology</span></h5>
-              <p class="text-muted mb-2 fw-bold">January 2023 - Present<br/>Sierra Vista, Arizona</p>
-              <p class="text-muted">
+              <p class="text mb-2 fw-bold">January 2023 - Present<br/>Sierra Vista, Arizona</p>
+              <p class="text">
                 • Resolved problems associated with SQL server and SharePoint system data<br/>
                 • Gathered data, designs formats, and created new programs or adjusted existing <br/>
                 &nbsp;&nbsp;programs as needed to create reports while documenting all changes<br/>
@@ -25,8 +25,8 @@ export const Experience = () => {
         
             <li class="timeline-item mb-5">
               <h5 class="fw-bold">Mechanical Engineer - <span>ISO Group</span></h5>
-              <p class="text-muted mb-2 fw-bold">August 2022 - December 2022<br/>Zoltbommel, Netherlands</p>
-              <p class="text-muted">
+              <p class="text mb-2 fw-bold">August 2022 - December 2022<br/>Zoltbommel, Netherlands</p>
+              <p class="text">
                 • Proposed robotic solutions to technical and performance issues<br/>
                 • Ensured fabricated robots met all company standards<br/>
                 • Assist in verifications and hardware inspections to ensure the machine <br/>&nbsp;&nbsp;meets all technical 
@@ -39,8 +39,8 @@ export const Experience = () => {
         
             <li class="timeline-item mb-5">
               <h5 class="fw-bold">Front End Web Developer - <span>Prive Wealth Club</span></h5>
-              <p class="text-muted mb-2 fw-bold">July 2021 - July 2022<br/>Richmond, Virginia</p>
-              <p class="text-muted">
+              <p class="text mb-2 fw-bold">July 2021 - July 2022<br/>Richmond, Virginia</p>
+              <p class="text">
                 • Maintain websites and applications, including content updates, account <br/>&nbsp;&nbsp;administration, debugging, feature enhancements and documentations, <br/>&nbsp;&nbsp;testing, and regular upgrades to ensure web security and system usability.<br/>
                 • Identify current internet and website requirements, and participate in <br/>&nbsp;&nbsp;planning and execution of projects by organizing, scheduling, executing, <br/>&nbsp;&nbsp;and coordinating work assignments to achieve milestone deadlines.<br/>
                 • Support maintenance programming development for web pages and <br/>&nbsp;&nbsp;web applications.<br/>
@@ -50,8 +50,8 @@ export const Experience = () => {
         
             <li class="timeline-item mb-5">
               <h5 class="fw-bold">IT Help Desk Technician - <span>GPM Investments, LLC</span></h5>
-              <p class="text-muted mb-2 fw-bold">March 2021 - June 2021<br/>Richmond, Virginia</p>
-              <p class="text-muted">
+              <p class="text mb-2 fw-bold">March 2021 - June 2021<br/>Richmond, Virginia</p>
+              <p class="text">
                 • Assist in the architecture, implementation and maintenance of the <br/>&nbsp;&nbsp;organization's desktop infrastructure environments including installation, <br/>&nbsp;&nbsp;upgrades, high level troubleshooting, patching, and software deployment<br/>
                 • Install Windows 10 Base Image, crafting the operating system and GPO <br/>&nbsp;&nbsp;settings so that they integrate with all enterprise hardware and software.<br/>
                 • Serve as the first point of contact for customers seeking technical <br/>&nbsp;&nbsp;assistance over the phone or email<br/>
